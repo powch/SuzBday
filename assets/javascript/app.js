08f358ea794b669386009034355b51fdc7812ef5
Chace Powell <cpcpowell8@gmail.com>
2018-08-04 22:03:32 -0400

    stuff

---
+++ assets/javascript/app.js
@@ -0,0 +1,21 @@
+var low = $('#low');
+var med = $('#med');
+var high = $('#high');
+var extra = $('#extra');
+
+var itemObj = {
+    price: {
+        low: {
+
+        },
+        med: {
+
+        }, 
+        high: {
+
+        },
+        extra: {
+            
+        }
+    }
+}
